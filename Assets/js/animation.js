f7c2519a958ba2f7f04340c1f1317d01388dbc5c
Salome <mbulambithe@gmail.com>
2023-02-18 11:34:00 +0300

    smallchanges

--- Assets/js/animation.js
+++ Assets/js/animation.js
@@ -4,7 +4,7 @@ anime.timeline({loop: true})
     opacity: [0.5,1],
     scaleX: [0, 1],
     easing: "easeInOutExpo",
-    duration: 700
+    duration: 1000
   }).add({
     targets: '.ml5 .line',
     duration: 600,
@@ -15,21 +15,21 @@ anime.timeline({loop: true})
     opacity: [0,1],
     scaleY: [0.5, 1],
     easing: "easeOutExpo",
-    duration: 600,
+    duration: 1000,
     offset: '-=600'
   }).add({
     targets: '.ml5 .letters-left',
     opacity: [0,1],
     translateX: ["0.5em", 0],
     easing: "easeOutExpo",
-    duration: 600,
+    duration: 1000,
     offset: '-=300'
   }).add({
     targets: '.ml5 .letters-right',
     opacity: [0,1],
     translateX: ["-0.5em", 0],
     easing: "easeOutExpo",
-    duration: 600,
+    duration: 1000,
     offset: '-=600'
   }).add({
     targets: '.ml5',
